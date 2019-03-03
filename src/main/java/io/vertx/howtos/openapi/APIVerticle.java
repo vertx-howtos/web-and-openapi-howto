578a8ea305f713dd31e60e2d2da06d3c4fd2a1b5
slinkydeveloper <francescoguard@gmail.com>
2019-03-03 12:26:29 +0100

    More referenced code
    Signed-off-by: francesco <francescoguard@gmail.com>

--- src/main/java/io/vertx/howtos/openapi/APIVerticle.java
+++ src/main/java/io/vertx/howtos/openapi/APIVerticle.java
@@ -35,52 +35,70 @@ public class APIVerticle extends AbstractVerticle {
         OpenAPI3RouterFactory routerFactory = ar.result();
 
         // Add routes handlers
+        // tag::listPetsHandler[]
         routerFactory.addHandlerByOperationId("listPets", routingContext ->
           routingContext
             .response() // <1>
             .setStatusCode(200)
-            .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-            .end(new JsonArray(getAllPets()).encode())
+            .putHeader(HttpHeaders.CONTENT_TYPE, "application/json") // <2>
+            .end(new JsonArray(getAllPets()).encode()) // <3>
         );
+        // end::listPetsHandler[]
+        // tag::createPetsHandler[]
         routerFactory.addHandlerByOperationId("createPets", routingContext -> {
-          RequestParameters params = routingContext.get("parsedParameters");
-          JsonObject pet = params.body().getJsonObject();
+          RequestParameters params = routingContext.get("parsedParameters"); // <1>
+          JsonObject pet = params.body().getJsonObject(); // <2>
           addPet(pet);
           routingContext
             .response()
             .setStatusCode(200)
-            .end();
+            .end(); // <3>
         });
+        // end::createPetsHandler[]
+        // tag::showPetByIdHandler[]
         routerFactory.addHandlerByOperationId("showPetById", routingContext -> {
-          RequestParameters params = routingContext.get("parsedParameters");
-          Integer id = params.pathParameter("petId").getInteger();
-          Optional<JsonObject> pet = getAllPets().stream().filter(p -> p.getInteger("id").equals(id)).findFirst();
+          RequestParameters params = routingContext.get("parsedParameters"); // <1>
+          Integer id = params.pathParameter("petId").getInteger(); // <2>
+          Optional<JsonObject> pet = getAllPets()
+            .stream()
+            .filter(p -> p.getInteger("id").equals(id))
+            .findFirst(); // <3>
           if (pet.isPresent())
             routingContext
               .response()
               .setStatusCode(200)
               .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-              .end(pet.get().encode());
+              .end(pet.get().encode()); // <4>
           else
-            routingContext.fail(404, new Exception("Pet not found"));
+            routingContext.fail(404, new Exception("Pet not found")); // <5>
         });
+        // end::showPetByIdHandler[]
 
         // Generate the router
-        Router router = routerFactory.getRouter();
-        router.errorHandler(404, routingContext -> {
-          JsonObject errorObject = new JsonObject()
+        // tag::routerGen[]
+        Router router = routerFactory.getRouter(); // <1>
+        router.errorHandler(404, routingContext -> { // <2>
+          JsonObject errorObject = new JsonObject() // <3>
             .put("code", 404)
-            .put("message", (routingContext.failure() != null) ? routingContext.failure().getMessage() : "Not Found");
+            .put("message",
+              (routingContext.failure() != null) ?
+                routingContext.failure().getMessage() :
+                "Not Found"
+            );
           routingContext
             .response()
             .setStatusCode(404)
             .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-            .end(errorObject.encode());
+            .end(errorObject.encode()); // <4>
         });
         router.errorHandler(400, routingContext -> {
           JsonObject errorObject = new JsonObject()
             .put("code", 400)
-            .put("message", (routingContext.failure() != null) ? routingContext.failure().getMessage() : "Validation Exception");
+            .put("message",
+              (routingContext.failure() != null) ?
+                routingContext.failure().getMessage() :
+                "Validation Exception"
+            );
           routingContext
             .response()
             .setStatusCode(400)
@@ -88,8 +106,9 @@ public class APIVerticle extends AbstractVerticle {
             .end(errorObject.encode());
         });
 
-        server = vertx.createHttpServer(new HttpServerOptions().setPort(8080).setHost("localhost"));
-        server.requestHandler(router).listen();
+        server = vertx.createHttpServer(new HttpServerOptions().setPort(8080).setHost("localhost")); // <5>
+        server.requestHandler(router).listen(); // <6>
+        // end::routerGen[]
         future.complete(); // Complete the verticle start
       } else {
         future.fail(ar.cause()); // Fail the verticle start
@@ -115,4 +134,19 @@ public class APIVerticle extends AbstractVerticle {
     this.pets.add(pet);
   }
 
+  // tag::loadSpecSampleMethod[]
+  // For documentation purpose
+  private void loadSpecSample(Future<Void> future) {
+    // tag::loadSpec[]
+    OpenAPI3RouterFactory.create(this.vertx, "petstore.yaml", ar -> {
+      if (ar.succeeded()) {
+        OpenAPI3RouterFactory routerFactory = ar.result(); // <1>
+      } else {
+        // Something went wrong during router factory initialization
+        future.fail(ar.cause()); // <2>
+      }
+    });
+    // end::loadSpec[]
+  }
+  // end::loadSpecSampleMethod[]
 }
